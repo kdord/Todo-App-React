c0f82572b0e018db12d870915e0e0db5220b2683
kdord <katya.koliada49@gmail.com>
2019-12-16 21:05:09 +0200

    add all components and styling

---
+++ src/NewTodoForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import uuid from "uuid/v4";
+import "./NewTodoForm.css";
+
+class NewTodoForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(evt) {
+    this.setState({
+      [evt.target.name]: evt.target.value
+    });
+  }
+  handleSubmit(evt) {
+    evt.preventDefault();
+
+    this.props.createTodo({ ...this.state, id: uuid(), completed: false });
+    this.setState({ task: "" });
+  }
+
+  render() {
+    return (
+      <form className="NewTodoForm" onSubmit={this.handleSubmit}>
+        <label htmlFor="task">New Todo</label>
+        <input
+          id="task"
+          type="text"
+          placeholder="New Todo"
+          value={this.state.task}
+          onChange={this.handleChange}
+          name="task"
+        />
+        <button>Add Todo</button>
+      </form>
+    );
+  }
+}
+
+export default NewTodoForm;
